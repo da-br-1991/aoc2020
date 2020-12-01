70eac9798682c0a764267004094fba2b6ce1351b
da-br-1991 <daniel.b91@gmx.de>
2020-12-01 12:08:52 +0100

    Initial commit

---
+++ aoc2020/src/main/java/de/adventOfCode/aoc2020/Aoc2020Application.java
@@ -0,0 +1,13 @@
+package de.adventOfCode.aoc2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aoc2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aoc2020Application.class, args);
+	}
+
+}
---
+++ aoc2020/src/test/java/de/adventOfCode/aoc2020/Aoc2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.adventOfCode.aoc2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aoc2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
